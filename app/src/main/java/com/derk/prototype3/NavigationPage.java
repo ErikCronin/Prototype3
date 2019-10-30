7277ce8e8108320b58f000c03273dd7fd2396432
ErikCronin <erik.cronin@my.jcu.edu.au>
2019-10-30 17:52:28 +1000

    completed navigation page

---
+++ app/src/main/java/com/derk/prototype3/NavigationPage.java
@@ -0,0 +1,14 @@
+package com.derk.prototype3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavigationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_page);
+    }
+}
